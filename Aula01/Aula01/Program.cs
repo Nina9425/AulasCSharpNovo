75a9498e987ab9b5038112688c8b883eaa643324
reyna.ncabezas <reyna.ncabezas@TIT0533396W10-1>
2025-02-25 19:27:36 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.Write("REYNA \n");
+Console.Write("NINA \n");
+
+
+//string nomeCompleto = "Reyna NIna C.";
+string firstName = "Reyna";
+string lastName = "Nina";
+string Surname = "Cabezas";
+
+Console.WriteLine("Meu nome completo é: " + firstName +" " + lastName+ " " + Surname);
+
+int idade = 30;
+
+
+Console.WriteLine($"Meu nome completo é:{firstName} {lastName} {Surname} é a minha idade {idade}");
+Console.WriteLine("escrevaseu nome");
+
+
+string nome = Console.ReadLine();                               //é para almacenar
+ 
+Console.WriteLine($"Olá {nome}, boa noite!!");
+
+
+
+
+
+
+
+
+//Console.ReadLine();
