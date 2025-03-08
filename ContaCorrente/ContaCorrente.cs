3b01af7e205efc6c3db3cfe98a87151ef4db6b8d
REYNA NINA CABEZAS <reyna.anyer.nina@gmail.com>
2025-03-07 21:04:41 -0300

    .

---
+++ ContaCorrente/ContaCorrente.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente
+{
+    internal class ContaCorrente
+    {
+        public int Numero;
+        public string Titular;
+        public double Saldo;
+
+        public ContaCorrente(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+        public void getDepositar(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+                Console.WriteLine($"Depósito de valor no{valor}, agora tem salde de:  {Saldo} ");
+            }
+            else
+            {
+                Console.WriteLine("Monto invalido - Não se adiciono nenhum valor ");
+            }
+            ExibirSaldo();
+            
+
+            }
+        public void Sacar(double valor)
+        {
+            if (valor > 0 && valor<=Saldo)
+            {
+                Saldo -= valor;
+                Console.WriteLine($"Saque de valor {Saldo}");
+            }
+            else
+            {
+                Console.WriteLine("monto a retirar é insuficiente.");
+            }
+
+            ExibirSaldo();
+        
+            }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Saldo actual de {Titular}: {Saldo}");
+        }
+
+
+
+
+    }
+}
---
+++ ContaCorrente/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ContaCorrente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente cuenta1 = new ContaCorrente(12345, "Reyna Nina", 1000);
+            cuenta1.ExibirSaldo();
+            cuenta1.getDepositar(15);
+            cuenta1.ExibirSaldo();
+
+
+            
+
+        }
+    }
+}
